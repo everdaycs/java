d2551a0e3208e21ed7104d4134d994ad04296cee
Yancheng <wjscycdsg@gmail.com>
2023-07-23 11:40:28 +0800

    初识Java

---
+++ JV23_4_9/src/Test.java
@@ -0,0 +1,26 @@
+
+
+public class Test {
+    // main
+
+    /**
+     * 这是一个main方法
+     * @param args 这是参数
+     */
+    // a 就是一个变量 --> 局部变量: 定义在方法内部的变量, 叫做局部变
+    // 变量都是有类型的, a 的类型-> a的数据类型[int] 占用的空间是4个字节
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+/*    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+//    字面常量
+//    char 是两个字节*/
+    // java
+}
