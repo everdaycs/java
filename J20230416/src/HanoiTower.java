0bc1b112e4fa32be6cba9ba381382a66eb5e27ed
Yancheng <wjscycdsg@gmail.com>
2023-07-25 14:24:58 +0800

    方法+数组

--- J20230416/src/HanoiTower.java
+++ J20230416/src/HanoiTower.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class HanoiTower {
+    public static void hannoi(int n , char a, char b, char c) {
+        if (n == 1) {
+            System.out.println(a + "----->" + c);
+        }else {
+            hannoi(n - 1, a, c, b);
+            hannoi(1, a, b, c);
+            hannoi(n - 1, b ,a ,c);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int left = scanner.nextInt();
+        System.out.println("将A座上的" + left + "个盘子全部移动到C的步骤为：");
+        hannoi(left, 'A', 'B', 'C');
+    }
+}
